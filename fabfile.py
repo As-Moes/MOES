badb551f25e8be8772a29c97a33faf2d523f97d8
Lozavival <lozavival@gmail.com>
2024-05-26 16:54:33 -0300

    Move hand_tracker code into function and add to fabfile

--- fabfile.py
+++ fabfile.py
@@ -5,9 +5,15 @@ import yaml
 from fabric import task
 
 from src.preprocess import prepare_data
+from src.scripts import hand_tracker
 
 @task
 def PrepareData(c):
     prepare_data.convert_videos_to_csv()
  
 
+@task
+def TrackHands(c):
+    width = 1080
+    height = 900
+    hand_tracker.track_hands(width, height)
--- src/scripts/hand_tracker.py
+++ src/scripts/hand_tracker.py
@@ -2,46 +2,47 @@ import cv2
 import mediapipe as mp
 
 # Initialize mediapipe hands module
-
 mphands   = mp.solutions.hands
 mpdrawing = mp.solutions.drawing_utils
 
-# Initialize video capture
-vidcap = cv2.VideoCapture(0)
-vidcap.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'))
 
-# Set the desired window width and height
-winwidth = 1080
-winheight = 900
+def mediapipe_detection(image, model):
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    image.flags.writeable = False
+    results = model.process(image)
+    image.flags.writeable = True
+    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+    return image, results
 
-# Initialize hand tracking
-with mphands.Hands(min_detection_confidence=0.5, min_tracking_confidence=0.5) as hands:
-    while vidcap.isOpened():
-        ret, frame = vidcap.read() 
-        if not ret:
-            break
-        
-        # Convert the BGR image to RGB
-        rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
-        # Process the frame for hand tracking
-        processFrames = hands.process(rgb_frame)
+def track_hands(winwidth: int, winheight: int) -> None:
+    vidcap = cv2.VideoCapture(0)
+    vidcap.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'))
 
-        # Draw landmarks on the frame
-        if processFrames.multi_hand_landmarks:
-            for lm in processFrames.multi_hand_landmarks:
-                mpdrawing.draw_landmarks(frame, lm, mphands.HAND_CONNECTIONS)
+    with mphands.Hands(min_detection_confidence=0.5, min_tracking_confidence=0.5) as hands:
+        while vidcap.isOpened():
+            ret, frame = vidcap.read()
+            if not ret:
+                break
 
-        # Resize the frame to the desired window size
-        resized_frame = cv2.resize(frame, (winwidth, winheight))
+            image, results = mediapipe_detection(frame, hands)
 
-        # Display the resized frame
-        cv2.imshow('Hand Tracking', resized_frame)
+            # Draw landmarks on the frame
+            if results.multi_hand_landmarks:
+                for lm in results.multi_hand_landmarks:
+                    mpdrawing.draw_landmarks(
+                        image, lm, mphands.HAND_CONNECTIONS)
 
-        # Exit loop by pressing 'q'
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
+            image = cv2.resize(image, (winwidth, winheight))
 
-# Release the video capture and close windows
-vidcap.release()
-cv2.destroyAllWindows()
+            cv2.imshow('Hand Tracking', cv2.flip(image, 1))
+
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+
+    vidcap.release()
+    cv2.destroyAllWindows()
+
+
+if __name__ == "__main__":
+    track_hands(1080, 900)
